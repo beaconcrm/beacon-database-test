cd48f21176fe49ed84bbfbdfea06c07ccfc98807
Sam Corney <sam@beaconcrm.org>
2024-08-08 10:13:48 +0100

    Update query

--- query.sql
+++ query.sql
@@ -1,11 +1,4 @@
 SELECT
-  a.id AS account_id,
-  a.name AS account_name,
-  count(1) AS number_of_users
+  *
 FROM
-  accounts a
-  JOIN account_users au ON a.id = au.account_id
-GROUP BY
-  a.id
-HAVING
-  count(1) > 1
+  users;
